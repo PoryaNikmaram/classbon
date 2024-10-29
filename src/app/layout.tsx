1250454f890973f67d4ea60073b80fed18a7e6c5
PoryaNikmaram <nikmaramp@gmail.com>
2024-10-29 16:35:23 +0330

    feat(types):add component core types

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Figtree } from "next/font/google";
+import { Baloo_Bhaijaan_2 } from "next/font/google";
 import "./globals.css";
 import { Header } from "./_components/header";
 import { Footer } from "./_components/footer";
@@ -10,16 +11,23 @@ const figtree = Figtree({
   variable: "--font-figtree",
 });
 
+const bahoo = Baloo_Bhaijaan_2({
+  display: "swap",
+  weight: ["400", "500", "600", "700", "800"],
+  subsets: ["arabic"],
+  variable: "--font-bahoo",
+});
+
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" dir="rtl" className={`dark ${figtree.variable}`}>
+    <html dir="rtl" className={`dark ${bahoo.variable}`}>
       <body className="min-h-screen grid grid-rows-[80px_1fr_auto] font-bold uppercase  bg-white text-base-100 dark:bg-base-100 dark:text-base-content">
         <Header />
-        <div className="flex flex-1 bg-teal-600 text-center">{children}</div>
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
