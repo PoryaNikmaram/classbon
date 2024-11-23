107593497f34a4171de9aff46d9a1042246e1579
Porya <110332298+PoryaNikmaram@users.noreply.github.com>
2024-11-23 11:59:36 +0330

    Add newest blog posts service (#15)
    * feat(loading): add loading component
    
    * feat(test): add jest and test button component
    
    * feat(storybook): add jest tests to button storybook
    
    * feat(storybook): add jest tests to loading storybook
    
    * feat(jest): add home hero section component and snapshot test
    
    * feat(new-courses): get new courses service with fetch api
    
    * feat(icon):add icon pack svgs and convert to component by gulp
    
    * feat(price):implement price component with it tests and storybook
    
    * feat(features):implement features component
    
    * feat(posts):fetch blog posts services
    
    ---------
    
    Co-authored-by: ArashTaghavi <arashtaghavi69@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import { CourseSummary } from "@/types/course-summery.interface";
 import { CourseCardList } from "./(courses)/_components/course-card-list";
 import { homeFeatures } from "@/data/home-features";
 import Feature from "./_components/feature/feature";
+import { Button } from "./_components/button";
+import { IconArrowLeftFill } from "./_components/icons/icons";
+import { BlogPostSummary } from "@/types/blog-post-summary.interface";
+import { useMemo } from "react";
 
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   const res = await fetch(
@@ -13,8 +17,23 @@ async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   );
   return res.json();
 }
+
+async function getNewestPosts(count: number): Promise<BlogPostSummary[]> {
+  const res = await fetch(`https://api.classbon.com/api/blog/newest/${count}`);
+  return res.json();
+}
+
 export default async function Home() {
-  const newestCourses = await getNewestCourses(4);
+  const newestCoursesData = getNewestCourses(4);
+  const newestPostsData = getNewestPosts(4);
+
+  const [newestCourses, newestPosts] = await Promise.allSettled([
+    newestCoursesData,
+    newestPostsData,
+  ]);
+
+  console.log(newestPosts);
+
   return (
     <>
       <HomeHeroSection />
@@ -32,7 +51,48 @@ export default async function Home() {
             برای به‌روز موندن، یاد گرفتن نکته‌های تازه ضروری‌ه!
           </p>
         </div>
-        <CourseCardList courses={newestCourses} />
+        <CourseCardList
+          courses={
+            newestCourses.status === "fulfilled" ? newestCourses.value : []
+          }
+        />
+      </section>
+      <section className="px-2 my-40">
+        <div className="relative pt-0 text-center">
+          <div className="bg-primary pointer-events-none absolute left-1/2 aspect-square w-1/2 -translate-x-1/2 -top-96 rounded-full opacity-10 blur-3xl"></div>
+
+          <h2
+            lang="en"
+            className="gradient leading-[1.3] relative z-10 mx-auto inline-block text-[clamp(2rem,6vw,5.5rem)] font-black"
+          >
+            ReactJs & Next.js
+          </h2>
+          <p className="text-base-content/70  relative z-[2] py-4 m-auto md:text-3xl max-w-5xl font-light !leading-[1.7]">
+            ری‌اکت و نکست‌جی‌اس برترین کتابخونه‌های فرانت‌اند و یکه‌تاز دنیای
+            وب! پیشرفته‌ترین مباحث رو اینجا می تونی پیدا کنی. پس همین الان
+            یادگیری رو شروع کن ما هم از ابتدای مسیر با آموزش‌های تخصصی و کاملاً
+            کاربردی کنارت هستیم.
+          </p>
+          <div className="flex flex-col lg:flex-row items-center gap-3 justify-center">
+            <Button
+              variant="primary"
+              size="large"
+              className="mt-7"
+              animatedIcon={true}
+            >
+              دوره‌های ری اکت و نکست‌ جی‌اس
+              <IconArrowLeftFill fill="currentColor" />
+            </Button>
+            <Button
+              variant="neutral"
+              size="large"
+              className="mt-7"
+              animatedIcon={true}
+            >
+              مقالات ری اکت و نکست‌ جی‌اس
+            </Button>
+          </div>
+        </div>
       </section>
     </>
   );
