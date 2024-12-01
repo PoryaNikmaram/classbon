311ceeaf2f7e9de83db1bdd4b7d6d917e77cd204
Porya <110332298+PoryaNikmaram@users.noreply.github.com>
2024-12-01 17:00:52 +0330

    feat(tabs):implement-tab-component (#27)

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { API_URL } from "@/configs/global";
 import type { CourseDetails } from "@/types/course-details.interface";
 import { CourseAside } from "./_components/course-aside";
+import { Tab } from "@/types/tab.type";
+import { Tabs } from "@/app/_components/tabs";
 
 export async function generateStaticParams() {
   const slugs = await fetch(`${API_URL}/courses/slugs`).then((res) =>
@@ -23,6 +25,20 @@ export default async function CourseDetails({
 }) {
   const { slug } = params;
   const course = await getCourse(slug);
+  const tabs: Tab[] = [
+    {
+      label: "مشخصات دوره",
+      content: course.description,
+    },
+    {
+      label: "دیدگاه‌ها و پرسش",
+      content: "course comments",
+    },
+    {
+      label: "سوالات متداول",
+      content: "accordion components",
+    },
+  ];
   return (
     <div className="container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10">
       <div className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2   rounded-full opacity-10 blur-3xl"></div>
@@ -39,7 +55,9 @@ export default async function CourseDetails({
       <div className="col-span-10 xl:col-span-3">
         <CourseAside {...course} />
       </div>
-      <div className="col-span-10 xl:col-span-6 bg-info"></div>
+      <div className="col-span-10 xl:col-span-6">
+        <Tabs tabs={tabs} />
+      </div>
       <div className="col-span-10 xl:col-span-4 bg-warning"></div>
     </div>
   );
